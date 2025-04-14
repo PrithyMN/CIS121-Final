51234429d560d06d4eaf8e2a00b46e76ee489d5d
PrithyMN <PrithyCA@gmail.com>
2025-04-13 22:40:13 -0500

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -1,7 +1,7 @@
 import math
 class TicTacToe:
   def __init__(self):
-    self.board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]] #creates 3 by 3 board
+    self.board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]] #creates 3 by 3 empty board
   
   def clearBoard(self):
     self.board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
@@ -12,21 +12,24 @@ class TicTacToe:
     else:
       self.board[xPos][yPos] = playerNum #square is claimed by player
 
+  #functions is to make checking for wins easy
   def checkValuesForWin(self, val1: int, val2: int, val3: int):
     return val1 == val2 == val3 and val1 != 0
   
   def checkForWin(self):
-
+    #checks for wins along rows
     for curRow in self.board: #iterates through each row
       if self.checkValuesForWin(curRow[0], curRow[1], curRow[2]): #checks if all values in the row are the same
         return curRow[0] #returns the number of the player who has won the game
-      
+    
+    #checks for wins along columns
     for curCol in range(3): #iterates through each column
       if self.checkValuesForWin(self.board[0][curCol], self.board[1][curCol], self.board[2][curCol]):
         return self.board[0][curCol] #returns the number of the player who has won the game
       
-    if self.checkValuesForWin(self.board[0][0], self.board[1][1], self.board[2][2]): #checks first diagonal
+    #checks for wins along diagonals
+    if self.checkValuesForWin(self.board[0][0], self.board[1][1], self.board[2][2]): #diagonal 1
       return self.board[0][0] #returns the number of the player who has won the game
-    if self.checkValuesForWin(self.board[0][2], self.board[1][1], self.board[2][0]): #checks second diagonal
+    if self.checkValuesForWin(self.board[0][2], self.board[1][1], self.board[2][0]): #diagonal 2
       return self.board[0][2] #returns the number of the player who has won the game
 
