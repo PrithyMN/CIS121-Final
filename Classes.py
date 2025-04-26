c18d7f4f94f51433d075b82b1bfd68aa4dcbbb05
PrithyMN <PrithyCA@gmail.com>
2025-04-25 20:27:28 -0500

    yep

--- Classes.py
+++ Classes.py
@@ -36,4 +36,49 @@ class TicTacToe:
       return self.board[0][0] #returns the number of the player who has won the game
     if self.checkValuesForWin(self.board[0][2], self.board[1][1], self.board[2][0]): #diagonal 2
       return self.board[0][2] #returns the number of the player who has won the game
+    
+  def printBoard(self):
+        symbols = {0: '.', 1: 'X', 2: 'O'} #creates dictionary to convert board values to prinable icons (0, 1, 2 -> ., X, O)
+        print("\nCurrent board:")
+        for row in self.board:
+            print(" ".join(symbols[cell] for cell in row))
+        print('') #skips line
+    
+'''
+    def playGame(self):
+        game = TicTacToe()
+        currentPlayer = 1
+
+        while True:
+            game.printBoard()
+            print(f"Player {currentPlayer}'s turn (X if 1, O if 2).")
+
+            try: #try statement for proper and improper inputs. Loops until a valid input is entered.
+                xPos = int(input("Enter row (0-2): "))
+                yPos = int(input("Enter column (0-2): "))
+                if xPos < 0 or xPos > 2 or yPos < 0 or yPos > 2:
+                    print("Invalid input! Row and column must be between 0 and 2.") #detects if input is out of range and tells player
+                    continue
+            except ValueError:
+                print("Invalid input! Please enter integers.")
+                continue
+
+            if not game.addValue(xPos, yPos, currentPlayer): #adds the player input to the board. if the square already has a value, the player is told to retry
+                print("Square has already been claimed, try again!")
+                continue #(Data Collection Type #3 Tuple^)
+
+            winner = game.checkForWin()
+            if winner == 1 or winner == 2:
+                game.printBoard()
+                print(f"Player {winner} wins!") #identifies higher value to assign winner and returns result
+                break
+            elif game.isFull():
+                game.printBoard() #calling printBorard function to show the game board
+                print("It's a tie!")
+                break
+
+            currentPlayer += 1
+            if currentPlayer >= 3: #eliminates possible weirdness if value is over 3 (but gives player 1 an advantage :) ?
+                currentPlayer = 1
+    '''
 
